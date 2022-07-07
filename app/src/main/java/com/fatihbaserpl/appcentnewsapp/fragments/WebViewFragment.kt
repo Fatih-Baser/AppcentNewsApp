2f33a2e7e76fc053870b5b4ba1e8e5588b0db829
Fatih Baser <basese_aehit@students.vizja.pl>
2022-07-07 16:49:40 +0300

    Webview fragment completed

--- app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/WebViewFragment.kt
+++ app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/WebViewFragment.kt
@@ -5,56 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.fatihbaserpl.appcentnewsapp.R
+import com.fatihbaserpl.appcentnewsapp.databinding.FragmentWebViewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class WebViewFragment : BaseFragment<FragmentWebViewBinding>(FragmentWebViewBinding::inflate) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WebViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class WebViewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val args: WebViewFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        webViewSetup()
+        backStackWeb()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WebViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WebViewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun backStackWeb() {
+        val callBack = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
             }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
+    }
+
+    private fun webViewSetup() {
+        binding.webview.webViewClient = WebViewClient()
+
+        binding.webview.apply {
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            loadUrl(args.webArgs.url)
+        }
     }
 }
