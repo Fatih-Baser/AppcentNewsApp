b6ce4f7e46ec12d6b8adf8cf36b133daa13f8f4f
Fatih Baser <basese_aehit@students.vizja.pl>
2022-07-07 16:07:22 +0300

    news fragment completed

--- app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/NewsFragment.kt
+++ app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/NewsFragment.kt
@@ -1,24 +1,96 @@
 package com.fatihbaserpl.appcentnewsapp.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fatihbaserpl.appcentnewsapp.NewsAdapter
 import com.fatihbaserpl.appcentnewsapp.R
+import com.fatihbaserpl.appcentnewsapp.databinding.FragmentNewsBinding
+import com.fatihbaserpl.appcentnewsapp.util.Resource
+import dagger.hilt.android.AndroidEntryPoint
 
 
-class NewsFragment : Fragment() {
+@AndroidEntryPoint
+class NewsFragment : BaseFragment<FragmentNewsBinding>(FragmentNewsBinding::inflate) {
 
+    private var newsAdapter = NewsAdapter()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+    private val viewModel: NewsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+        observeData()
+        swipeRefreshData()
     }
 
+    private fun setUpRecyclerView() {
+        binding.recyclerviewList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = newsAdapter
+            setRecycledViewPool(RecyclerView.RecycledViewPool())
+            setHasFixedSize(true)
+        }
+    }
+    private fun observeData() {
+        viewModel.newsResponse.observe(viewLifecycleOwner, { result ->
 
+            when (result) {
+                is Resource.Success -> {
+                    hideShimmerEffect()
+                    result.data?.articles.also {
+                        if (it != null) {
+                            newsAdapter.news = it
+                        }
+                    }
+                }
+                is Resource.Error -> {
+                    hideShimmerEffect()
+                    result.message?.let { message ->
+                        Log.e("TAG", "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
 
+    private fun swipeRefreshData() {
+        binding.swipe.setProgressBackgroundColorSchemeColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.white
+            )
+        )
+        binding.swipe.setColorSchemeColors(Color.RED)
+
+        binding.swipe.setOnRefreshListener {
+            observeData()
+            Toast.makeText(requireContext(), "Refresh From API", Toast.LENGTH_SHORT).show()
+            binding.swipe.isRefreshing = false
+        }
+    }
+
+    private fun showShimmerEffect() {
+        binding.shimmer.startShimmer()
+        binding.shimmer.visibility = View.VISIBLE
+        binding.recyclerviewList.visibility = View.GONE
+    }
+
+    private fun hideShimmerEffect() {
+        binding.shimmer.stopShimmer()
+        binding.shimmer.visibility = View.GONE
+        binding.recyclerviewList.visibility = View.VISIBLE
+    }
 }
