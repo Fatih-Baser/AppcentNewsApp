e8027edee3038f69c36831374a7c69c1f1a4339c
Fatih Baser <basese_aehit@students.vizja.pl>
2022-07-08 20:46:20 +0300

    Favorite is finished

--- app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/FavoriteFragment.kt
@@ -1,28 +1,58 @@
 package com.fatihbaserpl.appcentnewsapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fatihbaserpl.appcentnewsapp.NewsAdapter
 import com.fatihbaserpl.appcentnewsapp.R
 import com.fatihbaserpl.appcentnewsapp.databinding.FragmentFavoriteBinding
 import com.fatihbaserpl.appcentnewsapp.databinding.FragmentNewsBinding
+import com.fatihbaserpl.appcentnewsapp.favoriteroom.ArticleDatabase
+import com.fatihbaserpl.appcentnewsapp.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FavoriteFragment : Fragment() {
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate) {
+
+    private var favoriteNewsAdapter = FavoriteNewsAdapter()
+
+    private val viewModel: FavoriteViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+         observeLiveData()
 
 
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val binding = FragmentFavoriteBinding.inflate(inflater, container, false)
-        context ?:  return binding.root
-        return binding.root
+
+    fun observeLiveData() {
+        viewModel.getSavedNews().observe(viewLifecycleOwner, Observer {
+            it?.let {
+
+                favoriteNewsAdapter.news = it
+            }
+
+        })
+
+    }
+
+
+    private fun setUpRecyclerView() {
+        binding.favoriteRv.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = favoriteNewsAdapter
+            setRecycledViewPool(RecyclerView.RecycledViewPool())
+            setHasFixedSize(true)
+        }
     }
 
 
--- app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/FavoriteViewModel.kt
+++ app/src/main/java/com/fatihbaserpl/appcentnewsapp/fragments/FavoriteViewModel.kt
@@ -2,11 +2,15 @@ package com.fatihbaserpl.appcentnewsapp.fragments
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fatihbaserpl.appcentnewsapp.domain.Article
 import com.fatihbaserpl.appcentnewsapp.domain.NewsFavoriteRepository
+import com.fatihbaserpl.appcentnewsapp.domain.NewsResponse
+import com.fatihbaserpl.appcentnewsapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,15 +20,21 @@ class FavoriteViewModel @Inject constructor(
     val newsRepository : NewsFavoriteRepository
 ) : ViewModel(){
 
+    private val _response = MutableLiveData<List<Article>>()
 
     fun AddFavorite(article: Article) = viewModelScope.launch {
         newsRepository.addFavorite(article)
     }
     fun getSavedNews() = newsRepository.getAll()
 
+
+
+
     fun deleteFavorite(article: Article) = viewModelScope.launch {
         newsRepository.deleteFavorite(article)
     }
+
+
 
 
 
